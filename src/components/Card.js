b8441f6329e8819c7d986fdae44088ff041e95f1
AsyaCarroll <89799056+AsyaCarroll@users.noreply.github.com>
2023-11-22 17:35:40 +0300

    пр11

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,37 @@
 import trash from '../images/trash.svg'
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import { useContext } from 'react';
 
 function Card(props) {
+    const currentUser = useContext(CurrentUserContext);
+    const isOwn = props.card.owner._id === currentUser._id;
+    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    const cardLikeButtonClassName = (
+        `element__like ${isLiked && 'element__like_active'}`
+    );
 
     function handleClick() {
         props.onCardClick(props.card);
     }
 
+    function handleDeleteClick() {
+        props.oncardDelete(props.card);
+    }
+
+    function handleLikeClick() {
+        props.onCardLike(props.card);
+    }
+
     return (
         <article className="element">
             <div src="#" alt="#" className="element__pic" onClick={handleClick} style={{ backgroundImage: `url(${props.card.link})` }} />
-            <button className="element__trash" type="button">
+            {isOwn && <button className="element__trash" onClick={handleDeleteClick} type="button">
                 <img src={trash} alt="удалить" />
-            </button>
+            </button>}
             <div className="element__description">
                 <h2 className="element__name">{props.card.name}</h2>
                 <div className="element__container">
-                    <button type="button" className="element__like" aria-label="Нравится" />
+                    <button type="button" className={cardLikeButtonClassName} onClick={handleLikeClick} aria-label="Нравится" />
                     <p className="element__count">{props.card.likes.length}</p>
                 </div>
             </div>
